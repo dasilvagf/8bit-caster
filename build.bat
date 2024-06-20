ebcdfade607e0a35cb601e2be1b44d2b7df2db24
Gabriel Felipe <tracerofphotons@gmail.com>
2024-06-20 06:18:31 -0300

    ST7735 Set Up - Day 03: Configuring register and learning the SPI protocol
    Adding support for Arduino specifics, so we can properly debug code.

--- build.bat
+++ build.bat
@@ -1,9 +1,10 @@
 @echo off
 
 :: Build and link
-@call avr-gcc -I. -Os -DF_CPU=8000000 -mmcu=attiny85 -c src/main.c -o main.o
-@call avr-gcc -DF_CPU=8000000 -mmcu=attiny85 -o obj.out main.o
-@call avr-objcopy -O ihex obj.out obj.hex
+@call avr-gcc -I../avr-lib/include -Os -DF_CPU=8000000 -mmcu=attiny85 -c src/main.c -o bin/main.o
 
-:: Flash the code to the Arduino Board
-@call avrdude -c arduino -p m328p  -P com6 -U flash:w:obj.hex
+:: Get the avrlib dir
+@call COPY ..\avr-lib\bin\libavr.a bin\
+
+@call avr-gcc -DF_CPU=8000000 -mmcu=attiny85 -o bin/obj.out bin/main.o -L%CD%/bin/ -l:libavr.a
+@call avr-objcopy -O ihex bin/obj.out bin/obj.hex
